6d2a2a77b009629d88f619945bce3cbab5216d65
PEDIHS <130468444+PEDIHS@users.noreply.github.com>
2026-09-10 01:03:03 +0330

    fix: isolate Zomorod theme storage from PasarGuard dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -35,7 +35,7 @@ if (normalizedBorderRadius) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="zomorod-theme">
       <App />
       <Toaster />
     </ThemeProvider>
